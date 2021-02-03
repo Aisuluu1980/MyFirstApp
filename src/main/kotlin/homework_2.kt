511d58e6c4409b10a2f6e0bc5e83960914fff548
Aisuluu1980 <aisuluueco2009@yandex.ru>
2021-02-03 10:39:00 +0600

    Added option with 'var' and input on console on homework_2.

--- src/main/kotlin/homework_2.kt
+++ src/main/kotlin/homework_2.kt
@@ -1,5 +1,17 @@
+
+// Использую val
 fun main(){
     val name = "Aisulu"
     val surname = "Djusupova"
-    println("Hello, I am $name $surname!" )
+    val fullName = "Hello, I am $name $surname!"
+    println(fullName)
 }
+
+//Использую var
+//fun main(args: Array<String>) {
+//
+//    println("Enter your name and surname")
+//    var (name, surname) = readLine()!!.split(' ') // !! this operator use for NPE(NullPointerException).
+//
+//    println("My name is $name $surname.")
+//}
